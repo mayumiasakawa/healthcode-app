2139c99d1103795df61c19db853f784ef4cdae5d
mayumiasakawa <m.a.mayumi214@gmail.com>
2020-11-26 17:59:39 +0900

    バリデーション変更

--- app/models/physicalfinding.rb
+++ app/models/physicalfinding.rb
@@ -1,10 +1,13 @@
 class Physicalfinding < ApplicationRecord
   
   validates :measuring_date, presence: true
-  validates :hiegh_or_weight_or_abdominal, presence: true
+  validates :height_or_weight_or_abdominal, presence: true
+  validates :height, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 200 }, format: {with: /\A[0-9]+\z/}
+  validates :weight, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 200 }, format: {with: /\A[0-9]+\z/}
+  validates :abdominal_circumference, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 200 }, format: {with: /\A[0-9]+\z/}
 
   private
-  def hiegh_or_weight_or_abdominal
+  def height_or_weight_or_abdominal
     :heigh.present? || :weight.present? || :abdominal_circumference.present?
   end
 
