eb668d07e3e7dfe77bcf1ad815c1b8cad10fcad6
mayumiasakawa <m.a.mayumi214@gmail.com>
2020-11-27 11:45:13 +0900

    バリデーション変更、テストコード導入
    ・バリデーション変更
    身長・体重・腹囲のうち１項目入力されればOKというバリデーション

--- spec/models/physicalfinding_spec.rb
+++ spec/models/physicalfinding_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Physicalfinding, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @physicalfinding = FactoryBot.build(:physicalfinding)
+  end
+  describe '身体特徴登録' do
+    context '新規登録がうまくいく時' do
+      it 'measuring_dateとweight 、height、abdominal_circumferenceのいずれが存在すれば登録できる' do
+        expect(@physicalfinding).to be_valid
+      end
+    end
+
+    context '新規登録がうまくいかない時' do
+      it 'measuring_dateが空では登録できない' do
+        @physicalfinding.measuring_date = ''
+        @physicalfinding.valid?
+        expect(@physicalfinding.errors.full_messages).to include("Measuring date can't be blank")
+      end
+      it 'weight 、height、abdominal_circumferenceが空では登録できない' do
+        @physicalfinding.weight = ''
+        @physicalfinding.height = ''
+        @physicalfinding.abdominal_circumference = ''
+        @physicalfinding.valid?
+        expect(@physicalfinding.errors.full_messages).to include("Height or weight or abdominal is invalid")
+      end
+    end
+  end
 end
