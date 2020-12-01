a6934a2bf4f1dbeaab910ba512abaab2594496c4
mayumiasakawa <m.a.mayumi214@gmail.com>
2020-12-01 14:51:04 +0900

    バリデーション追加

--- app/models/overview.rb
+++ app/models/overview.rb
@@ -5,7 +5,8 @@ class Overview < ApplicationRecord
   belongs_to :user, optional: true
   has_one_attached :image
 
-  validates :physicalfinding_measuring_date, presence: true, if: -> { height.present? || weight.present? || abdominal_circumference.present? }
+  validates :physicalfinding_measuring_date, presence: true,if: -> { (height.present? || weight.present? || abdominal_circumference.present?) ||
+                                                                     (blood_urine_test_date.blank? && medeical_care_date.blank? && vaccine_date.blank?) }
   validates :height, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 200 }, format: {with: /\A[0-9]+\z/},
              presence: true, if: -> { physicalfinding_measuring_date.present? && weight.present? && abdominal_circumference.present? }
   validates :weight, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 200 }, format: {with: /\A[0-9]+\z/},
@@ -14,26 +15,30 @@ class Overview < ApplicationRecord
              presence: true, if: -> { physicalfinding_measuring_date.present? && height.present? && weight.present? }
 
   validates :blood_urine_test_date,
-             presence: true, if: :was_attached?
+             presence: true, if: :was_attached?, if: -> { (image.present?) || (physicalfinding_measuring_date.blank? && medeical_care_date.blank? && vaccine_date.blank?) }
   validates :image,
              presence: true, if: -> { blood_urine_test_date.present? }
             
   validates :medeical_care_date,
-            presence: true, if: -> { (clinic_name.present? && disease_name.present?) || treatment_medicine.present? }
+             presence: true, if: -> { (clinic_name.present? && disease_name.present? && treatment_medicine.present?) ||
+                                      (physicalfinding_measuring_date.blank? && blood_urine_test_date.blank? && vaccine_date.blank?) }
   validates :clinic_name,
-            presence: true, if: -> { (medeical_care_date.present? && disease_name.present?) || treatment_medicine.present? }
+            presence: true, if: -> { medeical_care_date.present? && disease_name.present? && treatment_medicine.present? }
   validates :disease_name,
-            presence: true, if: -> { (medeical_care_date.present? && clinic_name.present?) || treatment_medicine.present? }
+            presence: true, if: -> { medeical_care_date.present? && clinic_name.present? && treatment_medicine.present? }
   validates :treatment_medicine,
-            presence: true, if: -> { (medeical_care_date.present? && clinic_name.present?) && disease_name.present? }
+            presence: true, if: -> { medeical_care_date.present? && clinic_name.present? && disease_name.present? }
 
-  validates :vaccine_date,presence: true, if: -> { vaccine_id.present? }
+  validates :vaccine_date,
+             presence: true, unless: -> { vaccine_id.nil? },
+                             if: -> { physicalfinding_measuring_date.blank? && blood_urine_test_date.blank? && medeical_care_date.blank? }
+                             
   validates :vaccine_id, numericality: { other_than: 1 } ,
-            presence: true, if: -> { vaccine_date.present? }
+             presence: true, if: -> { vaccine_date.present? }
 
 
- def was_attached?
-  self.image.attached?
- end
+  def was_attached?
+    self.image.attached?
+  end
 
 end
