c9d0732c0e04274973e9af1d2d4ff43bd8165dd5
mayumiasakawa <m.a.mayumi214@gmail.com>
2020-12-02 14:45:12 +0900

    index 最新データ表示

--- app/controllers/overviews_controller.rb
+++ app/controllers/overviews_controller.rb
@@ -2,6 +2,10 @@ class OverviewsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
+    @overview_physicalfinding_measuring = Overview.order(physicalfinding_measuring_date: :desc).first
+    @overview_blood_urine_test = Overview.order(blood_urine_test_date: :desc).first
+    @overview_medeical_cares = Overview.order(medeical_care_date: :desc).first(3)
+    @overview_vaccines = Overview.order(vaccine_date: :desc).first(3)
   end
 
   def new
