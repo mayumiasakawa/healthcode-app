ad100e6f74ed4092ff2ac11830dfbcf33af53ed8
mayumiasakawa <m.a.mayumi214@gmail.com>
2020-12-16 15:49:01 +0900

    chart ユーザーのみ

--- app/controllers/overviews_controller.rb
+++ app/controllers/overviews_controller.rb
@@ -81,18 +81,20 @@ class OverviewsController < ApplicationController
   end
 
   def set_chart
-    physicalfinding_measuring_date = Overview.where.not(physicalfinding_measuring_date:nil).order(physicalfinding_measuring_date: :asc).pluck(:physicalfinding_measuring_date)
-    weight = Overview.where.not(physicalfinding_measuring_date:nil).order(physicalfinding_measuring_date: :asc).pluck(:weight)
-    bmi = Overview.where.not(physicalfinding_measuring_date:nil).order(physicalfinding_measuring_date: :asc).pluck(:bmi)
-    abdominal_circumference = Overview.where.not(physicalfinding_measuring_date:nil).order(physicalfinding_measuring_date: :asc).pluck(:abdominal_circumference)
+    if user_signed_in?
+      physicalfinding_measuring_date = Overview.where.not(physicalfinding_measuring_date:nil).where(user_id: current_user.id).order(physicalfinding_measuring_date: :asc).pluck(:physicalfinding_measuring_date)
+      weight = Overview.where.not(physicalfinding_measuring_date:nil).where(user_id: current_user.id).order(physicalfinding_measuring_date: :asc).pluck(:weight)
+      bmi = Overview.where.not(physicalfinding_measuring_date:nil).where(user_id: current_user.id).order(physicalfinding_measuring_date: :asc).pluck(:bmi)
+      abdominal_circumference = Overview.where.not(physicalfinding_measuring_date:nil).where(user_id: current_user.id).order(physicalfinding_measuring_date: :asc).pluck(:abdominal_circumference)
 
-    @chart = LazyHighCharts::HighChart.new("graph") do |c|
-      # c.title(text: "体重・腹囲 推移")
-      c.xAxis(categories: physicalfinding_measuring_date, title: {text: '測定日'})
-      c.yAxis(title: {text: 'kg / cm'})
-      c.series(name: "腹囲 cm", data: abdominal_circumference)
-      c.series(name: "体重 kg", data: weight)
-      c.series(name: "BMI", data: bmi)
+      @chart = LazyHighCharts::HighChart.new("graph") do |c|
+        # c.title(text: "体重・腹囲 推移")
+        c.xAxis(categories: physicalfinding_measuring_date, title: {text: '測定日'})
+        c.yAxis(title: {text: 'kg / cm'})
+        c.series(name: "腹囲 cm", data: abdominal_circumference)
+        c.series(name: "体重 kg", data: weight)
+        c.series(name: "BMI", data: bmi)
+      end
     end
   end
 
